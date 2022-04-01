3f5a14e45a97fbfe98a464eae0aedd7b0758116b
erdogant <erdogant@gmail.com>
2022-04-01 15:41:42 +0200

    update version en meer

--- d3blocks/__init__.py
+++ d3blocks/__init__.py
@@ -8,7 +8,7 @@ from d3blocks.d3blocks import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # module level doc-string
 __doc__ = """
