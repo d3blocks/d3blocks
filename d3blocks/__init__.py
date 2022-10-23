a9ebcf40928918827f0d1469a08197705bd7185f
erdogant <erdogant@gmail.com>
2022-10-23 22:34:09 +0200

    new release with major code refactoring!

--- d3blocks/__init__.py
+++ d3blocks/__init__.py
@@ -11,7 +11,7 @@ from d3blocks.d3blocks import D3Blocks
 
 __author__ = 'Erdogan Taskesen, Oliver Verver'
 __email__ = 'erdogant@gmail.com, mail@oliver3.nl'
-__version__ = '1.0.10'
+__version__ = '1.1.0'
 
 # module level doc-string
 __doc__ = """
@@ -20,21 +20,7 @@ d3blocks
 
 Description
 -----------
-d3blocks is for the creation of exclusive stand alone and interactive graphs in d3 javascript.
-
-Example
--------
->>> from d3blocks import d3blocks
->>> #
->>> # Initialize
->>> d3 = D3Blocks()
->>> #
->>> # Load example data
->>> df = d3.import_example(graph='random_time', n=10000, c=300, date_start="2000-1-1 00:10:05", date_stop="2000-1-1 23:59:59")
->>> #
->>> # Plot
->>> d3.movingbubbles(df, speed={"slow": 1000, "medium": 200, "fast": 10})
->>> #
+d3blocks is for the creation of stand-alone and interactive d3 graphs.
 
 References
 ----------
