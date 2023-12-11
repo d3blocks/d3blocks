12fbca8827a781dbb82ba996d2227516634956cb
erdogant <erdogant@gmail.com>
2023-12-11 23:34:18 +0100

    1.4.9

--- d3blocks/__init__.py
+++ d3blocks/__init__.py
@@ -10,7 +10,7 @@ from d3blocks.utils import (
 
 __author__ = 'Erdogan Taskesen, Oliver Verver'
 __email__ = 'erdogant@gmail.com, mail@oliver3.nl'
-__version__ = '1.4.8'
+__version__ = '1.4.9'
 
 # module level doc-string
 __doc__ = """
