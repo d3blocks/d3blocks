d1a87a78cf6c40797478dc77a108a66ad3b8ad71
erdogant <erdogant@gmail.com>
2024-04-02 17:37:03 +0200

    update examples for chord

--- d3blocks/examples.py
+++ d3blocks/examples.py
@@ -6,6 +6,39 @@ import pandas as pd
 import numpy as np
 
 # %%
+# Load d3blocks
+from d3blocks import D3Blocks
+#
+# Initialize
+d3 = D3Blocks(chart='Chord', frame=False)
+#
+# Import example
+df = d3.import_example('energy')
+#
+# Node properties
+d3.set_node_properties(df, opacity=0.2, cmap='tab20')
+d3.set_edge_properties(df, color='source', opacity='source')
+#
+# Show the chart
+d3.show()
+#
+# Make some edits to highlight the Nuclear node
+# d3.node_properties
+d3.node_properties.get('Nuclear')['color']='#ff0000'
+d3.node_properties.get('Nuclear')['opacity']=1
+# Show the chart
+#
+d3.show()
+
+# Make edits to highlight the Nuclear Edge
+d3.edge_properties.loc[(d3.edge_properties['source'] == 'Nuclear') & (d3.edge_properties['target'] == 'Thermal generation'), 'color'] = '#ff0000'
+d3.edge_properties.loc[(d3.edge_properties['source'] == 'Nuclear') & (d3.edge_properties['target'] == 'Thermal generation'), 'opacity'] = 0.8
+d3.edge_properties.loc[(d3.edge_properties['source'] == 'Nuclear') & (d3.edge_properties['target'] == 'Thermal generation'), 'weight'] = 1000
+#
+# Show the chart
+d3.show()
+
+# %%
 from d3blocks import D3Blocks
 d3 = D3Blocks()
 
@@ -104,7 +137,7 @@ html = d3.sankey(df, filepath=r'c:\temp\sankey.html', fontsize=12)
 
 # %% Save functionality
 from d3blocks import D3Blocks
-d3 = D3Blocks()
+d3 = D3Blocks(support=False)
 
 # Violin
 df = d3.import_example('cancer')
@@ -142,7 +175,9 @@ html = d3.sankey(df, filepath=r'c:\temp\sankey.html', save_button=True)
 
 # particles
 df = d3.import_example('energy')
-html = d3.particles('D3blocks')
+from d3blocks import D3Blocks
+d3 = D3Blocks(support=False)
+html = d3.particles('Data en Innovatielab', fontsize=150, radius=2, spacing=6, figsize=[1500, 200], filepath=r'D:\OneDrive\DILAB\PROJECTEN\intakeformulieren\docs\pages\dilab.html')
 html = d3.particles('D3blocks', save_button=False)
 html = d3.particles('D3blocks', save_button=True)
 
