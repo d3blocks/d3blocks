bf9335676656b4253f0db82cbc36555c8f35df20
erdogant <erdogant@gmail.com>
2025-06-22 08:18:03 +0200

    1.5.1

--- d3blocks/__init__.py
+++ d3blocks/__init__.py
@@ -10,7 +10,7 @@ from d3blocks.utils import (
 
 __author__ = 'Erdogan Taskesen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.5.0'
+__version__ = '1.5.1'
 
 # Setup root logger
 import logging
