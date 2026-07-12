210b9000b99c21922d885a7f921d5c4e65534cf5
erdogant <erdogant@gmail.com>
2026-07-12 22:18:19 +0200

    new examples

--- d3blocks/examples_docs.py
+++ d3blocks/examples_docs.py
@@ -145,3 +145,72 @@ d3.d3graph(df,
            filepath=r'D:\REPOS\erdogant.github.io\docs\d3blocks\d3graph_socialmedia.html',
            )
 
+# %%
+# =============================================================================
+# CHORD
+# =============================================================================
+# Load d3blocks
+from d3blocks import D3Blocks
+#
+# Initialize
+d3 = D3Blocks()
+#
+# Load example data
+df = d3.import_example('energy')
+#
+# Plot
+d3.chord(df,
+         filepath=r'D:\REPOS\erdogant.github.io\docs\d3blocks\chord_energy1.html',
+         )
+#
+
+# =============================================================================
+# 
+# =============================================================================
+
+from d3blocks import D3Blocks
+#
+# Initialize
+d3 = D3Blocks()
+#
+# Load example data
+df = d3.import_example('energy')
+#
+# Plot with very large margins for long labels
+d3.chord(df,
+         filepath=r'D:\REPOS\erdogant.github.io\docs\d3blocks\chord_energy2.html',
+         # figsize=[650, 450],  # Even larger figure size
+         margin=300,  # Large margin for long labels
+         text_offset=50,  # Large text offset
+         )
+
+# =============================================================================
+# 
+# =============================================================================
+
+# Load d3blocks
+from d3blocks import D3Blocks
+#
+# Initialize
+d3 = D3Blocks(chart='Chord', frame=False)
+#
+# Import example
+df = d3.import_example('energy')
+#
+# Node properties
+d3.set_node_properties(df, opacity=0.2, cmap='tab20')
+d3.set_edge_properties(df, color='source', opacity='source')
+#
+
+# Make some edits to highlight the Nuclear node
+# d3.node_properties
+d3.node_properties.get('Nuclear')['color']='#ff0000'
+d3.node_properties.get('Nuclear')['opacity']=1
+# Make edits to highlight the Nuclear Edge
+d3.edge_properties.loc[(d3.edge_properties['source'] == 'Nuclear') & (d3.edge_properties['target'] == 'Thermal generation'), 'color'] = '#ff0000'
+d3.edge_properties.loc[(d3.edge_properties['source'] == 'Nuclear') & (d3.edge_properties['target'] == 'Thermal generation'), 'opacity'] = 0.8
+d3.edge_properties.loc[(d3.edge_properties['source'] == 'Nuclear') & (d3.edge_properties['target'] == 'Thermal generation'), 'weight'] = 1000
+#
+# Show the chart
+d3.show(filepath=r'D:\REPOS\erdogant.github.io\docs\d3blocks\chord_energy3.html')
+
