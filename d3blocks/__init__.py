029ec96b6dbdf6ceb60bba1ae0ed9e37e8937863
erdogant <erdogant@gmail.com>
2026-08-07 12:32:53 +0200

    make the bottom menu user sizable

--- d3blocks/__init__.py
+++ d3blocks/__init__.py
@@ -10,7 +10,7 @@ from d3blocks.utils import (
 
 __author__ = 'Erdogan Taskesen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.8.0'
+__version__ = '1.8.1'
 
 # Setup root logger
 import logging
