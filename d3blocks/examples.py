b0c2aed2395d11bcc1394653b4533fef2c730c99
erdogant <erdogant@gmail.com>
2026-09-10 00:20:01 +0200

    new release

--- d3blocks/examples.py
+++ d3blocks/examples.py
@@ -17,10 +17,7 @@ d3 = D3Blocks()
 d3.particles('D3Blocks', fontsize=60, radius=1, spacing=4, collision=0.05, mouse_radius=20, shape='square', cmap='Turbo', figsize=[900, 200])
 # d3.particles('D3Blocks', spacing=8, shape='square', square_scale=1.5)
 
-
 # %%
-
-
 from d3blocks import D3Blocks
 d3 = D3Blocks()
 # Default: mainland only
@@ -1133,8 +1130,6 @@ d3.D3graph.set_node_properties(color='cluster')
 
 # Plot
 d3.D3graph.show()
-
-
 
 
 # %%
